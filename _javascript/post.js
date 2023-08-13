4a962dfc4b239c7ad3c61fea3faaa294d28c14d0
orion_gz <asdfg60842@gmail.com>
2023-08-14 02:47:35 +0900

    Share BTN added

--- _javascript/post.js
+++ _javascript/post.js
@@ -15,3 +15,41 @@ imgPopup();
 initLocaleDatetime();
 initClipboard();
 toc();
+
+/*
+//Javascript
+var lastScrollTop = 0;
+var delta = 5;
+var fixBox = $('#topbar-wrapper');
+var fixBoxHeight = fixBox.offsetHeight;
+var didScroll;
+//스크롤 이벤트 
+window.onscroll = function(e) {
+    didScroll = true;
+};
+
+//0.25초마다 스크롤 여부 체크하여 스크롤 중이면 hasScrolled() 호출
+setInterval(function(){
+    if(didScroll){
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled(){
+    var nowScrollTop = window.scrollY;
+    if(Math.abs(lastScrollTop - nowScrollTop) <= delta){
+        return;
+    }
+    if(nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight){
+        //Scroll down
+        fixBox.removeClass('show');
+    }else{
+        if(nowScrollTop + window.innerHeight < document.body.offsetHeight){
+            //Scroll up
+            fixBox.addClass('show');
+        }
+    }
+    lastScrollTop = nowScrollTop;
+}
+*/
