3267eadee44b093361f2e0b541ccc00aa0bedee6
orion_gz <asdfg60842@gmail.com>
2023-08-16 23:42:08 +0900

    Plugin Edit

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 #
 # Check for changed posts
-
+module Jekyll
+  
 Jekyll::Hooks.register :posts, :post_init do |post|
 
   commit_num = `git rev-list --count HEAD "#{ post.path }"`
